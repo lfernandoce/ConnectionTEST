4ca54388484b52b06ada082d1e45c14002bdadd4
LFCE <luif.2110@gmail.com>
2020-11-21 02:46:11 -0600

    Coments

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace ConnectionTEST
     {
         static void Main(string[] args)
         {
+            //Ejemplo de una coneccion a SQLSERVER
             String StrCon = "data source=VSR-DEVCOOKIE\\DEVCOOKIE;initial catalog=MvcMovieContext;persist security info=True;user id=sa;password=LFCE_2110;MultipleActiveResultSets=True;App=ConnectionTEST";
             DbSQL db = new DbSQL(StrCon);
             db.Query = "SELECT * FROM TAB_USUARIOS_REGISTRADOS";
@@ -14,12 +15,12 @@ namespace ConnectionTEST
             db.Exec = DbSQL.Execute.Fill;
             db.CommandType = System.Data.CommandType.Text;
             db.EjecutarSQL();
+            System.Console.ReadKey();
 
 
 
 
-
-
+            //Ejemplo de coneccion a oracle, utilizamos oracle 10g
             string strConOra = "Data Source=localhost:1521; User Id=system;Password=2110;";
             DbORACLE Oradb = new DbORACLE(strConOra);
             Oradb.Query = "SELECT * FROM TAB_USUARIOS";
@@ -35,11 +36,10 @@ namespace ConnectionTEST
             System.Console.ReadKey();
 
 
-
+            // Ejemplo de coneccion a MySQL
             string strConMysql = "Server=localhost;Database=Test;Uid=root;Pwd='';";
             DbMySQL MySqldb = new DbMySQL(strConMysql);
-            MySqldb.Query = "SELECT * FROM USUARIOS_TEST";
-            //Oradb.Database = "xe";
+            MySqldb.Query = "SELECT * FROM USUARIOS_TEST";            
             MySqldb.Exec = DbMySQL.Execute.Fill;
             MySqldb.CommandType = System.Data.CommandType.Text;
             DataTable dt2 = new DataTable();
